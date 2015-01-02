99d42c782fe5a7c8d18c984ed0b875c94fea57b8
elkorn <helluinster@gmail.com>
2015-01-02 20:23:06 +0100

    Migrate Scala problems code to the new project.

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,7 +1,12 @@
 package org.p99.scala
 
+import scala.annotation.tailrec
+
 object P01 {
-
-  // TODO
-
+  @tailrec
+  def last[A](list: List[A]): Option[A] = list match {
+    case head :: Nil => Some(head)
+    case Nil => None
+    case _ => last(list.tail)
+  }
 }
--- src/main/scala/P02.scala
+++ src/main/scala/P02.scala
@@ -1,7 +1,12 @@
 package org.p99.scala
 
+import scala.annotation.tailrec
+
 object P02 {
-
-  // TODO
-
+  @tailrec
+  def penultimate[A](list: List[A]): Option[A] = list match {
+    case Nil => None
+    case penultimate :: List(last) => Some(penultimate)
+    case _ => penultimate(list.tail)
+  }
 }
