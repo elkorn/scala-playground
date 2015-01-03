fab8446fb1208f112488458f336e1b7279809925
elkorn <helluinster@gmail.com>
2015-01-03 21:43:46 +0100

    Add P04.

--- src/main/scala/com/s99/P04.scala
+++ src/main/scala/com/s99/P04.scala
@@ -1,7 +1,18 @@
 package com.s99
 
+import scala.annotation.tailrec
+
 object P04 {
 
-  // TODO
+  def length[A](list: List[A]): Int = {
+    @tailrec
+    def go[A](list: List[A], length: Int): Int = list match {
+      case Nil => length
+      case head :: Nil => length + 1
+      case head :: tail => go(tail, length+1)
+    }
+
+    go(list, 0)
+  }
 
 }
--- src/test/scala/P04Spec.scala
+++ src/test/scala/P04Spec.scala
@@ -1,10 +1,19 @@
 package org.p99.scala
 
+import com.s99.P04
 import org.scalatest._
 
-class P04Spec extends UnitSpec {
+class P04Spec extends UnitSpec with Matchers {
 
-  // TODO
+  it should "count the number of elements in a non-empty list" in {
+    val list = List(1, 2, 3, 4, 5, 6)
+    P04.length(list) should be(6)
+  }
+
+  it should "count the number of elements in an empty list" in {
+    val list = List()
+    P04.length(list) should be(0)
+  }2
 
 }
 
