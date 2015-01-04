d3be868665513f131dd9329eabfe6ecf0730fdff
elkorn <helluinster@gmail.com>
2015-01-04 15:48:43 +0100

    Add P06.

--- src/main/scala/com/s99/P06.scala
+++ src/main/scala/com/s99/P06.scala
@@ -1,7 +1,18 @@
 package com.s99
 
+import scala.annotation.tailrec
+
 object P06 {
 
-  // TODO
+  def isPalindrome[A](list: List[A]): Boolean = {
+    @tailrec
+    def go(left: Int, right: Int, list: List[A]): Boolean = {
+      if(left == right) true
+      else if (list.isDefinedAt(left) && list.isDefinedAt(right) && list(left) == list(right)) go(left+1, right-1, list)
+      else false
+    }
+
+    go(0, list.length-1, list)
+  }
 
 }
