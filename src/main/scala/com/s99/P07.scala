b2432cfe9bbc246477cd2dd0fb325cadb2de9b30
elkorn <helluinster@gmail.com>
2015-01-05 19:31:18 +0100

    Add P07.

--- src/main/scala/com/s99/P07.scala
+++ src/main/scala/com/s99/P07.scala
@@ -1,7 +1,21 @@
 package com.s99
 
+import scala.annotation.tailrec
+
 object P07 {
 
-  // TODO
+  def flatten(list: List[Any]): List[Any] = {
+    @tailrec
+    def go(result: List[Any], rest: List[Any]): List[Any] = rest match {
+      case head :: tail => head match {
+        case x: List[Any] => go(result ::: flatten(x), tail)
+        case x: Any => go(result :+ x, tail)
+      }
+
+      case Nil => result
+    }
+
+    go(Nil, list)
+  }
 
 }
--- src/test/scala/P07Spec.scala
+++ src/test/scala/P07Spec.scala
@@ -1,10 +1,14 @@
 package org.p99.scala
 
+import com.s99.P07
 import org.scalatest._
 
-class P07Spec extends UnitSpec {
+class P07Spec extends UnitSpec with Matchers {
 
-  // TODO
+  it should "flatten a list" in {
+    P07.flatten(List(List(1,2,3), List(4,5,6, List(7,8,9)))) should be(List(1,2,3,4,5,6,7,8,9))
+    P07.flatten(List(List(List(1), List(2, List(3))))) should be(List(1,2,3))
+  }
 
 }
 
