9a8d53471894492e0fd3f30a8656e1266464a77a
Korneliusz Caputa <helluinster@gmail.com>
2015-01-10 18:28:52 +0100

    Add `or` combinator.

--- src/main/scala/com/fpinscala/parsing/Parsers.scala
+++ src/main/scala/com/fpinscala/parsing/Parsers.scala
@@ -7,10 +7,11 @@ package com.fpinscala.parsing
 trait Parsers[ParseError, Parser[+ _]] {
   def char(c: Char): Parser[Char] = ???
 
+  def orString(s1: String, s2: String): Parser[String] = or(string(s1), string(s2))
+
   def string(s: String): Parser[String] = ???
 
-  def orString(s1: String, s2: String): Parser[String] = ???
-
+  def or[A](p1: Parser[A], p2: Parser[A]): Parser[A] = ???
 
   def run[A](p: Parser[A])(input: String): Either[ParserError, A] = ???
 }
