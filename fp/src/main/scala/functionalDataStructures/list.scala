201640f7b00d5efea0fa388dcb70968384fb6dce
Korneliusz Caputa <helluinster@gmail.com>
2015-04-29 20:04:08 +0200

    Add the list exercises. magit has failed me :(

---
+++ fp/src/main/scala/functionalDataStructures/list.scala
@@ -0,0 +1,115 @@
+package fp.functionalDataStructures
+
+import scala.annotation.tailrec
+
+/*
+    +A - covariant -> if X is a subtype of Y, then List[X] is a
+    subtype of List[Y].
+   -A - contravariant -> if X is a subtype of Y then List[X] is a supertype of List[Y].
+  */
+sealed trait List[+A] {
+  def tail(): List[A] = this.drop(1)
+  def drop(n: Int): List[A] = {
+    if (n <= 0) this
+    else this match {
+      case Nil => sys.error("drop on empty list")
+      case Cons(_, tail) => tail.drop(n - 1)
+    }
+  }
+}
+
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, rest: List[A]) extends List[A]
+
+object List {
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, List(as.tail: _*))
+
+  def setHead[A](list: List[A], newHead: A): List[A] = list match {
+    case Nil => Nil
+    case Cons(_, tail) => Cons(newHead, tail)
+  }
+
+  @tailrec
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(head, tail) if (f(head)) => dropWhile(tail)(f)
+    case list => list
+  }
+
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(head, Nil) => Nil
+    case Cons(head, tail) => Cons(head, init(tail))
+  }
+
+  /*
+     Right-associative folding.
+     */
+  def foldRight[A, B](as: List[A], zero: B)(f: (A, B) => B): B = {
+    println(as)
+    as match {
+      case Nil => zero
+      case Cons(head, tail) => f(head, foldRight(tail, zero)(f))
+    }
+  }
+
+  def length[A](l: List[A]): Int = foldRight(l, 0)((a, b) => b + 1)
+
+  /*
+     Left-associative folding.
+     */
+  @tailrec
+  def foldLeft[A, B](as: List[A], zero: B)(f: (B, A) => B): B = {
+    println(as)
+    as match {
+      case Nil => zero
+      case Cons(head, tail) => foldLeft(tail, f(zero, head))(f)
+    }
+  }
+
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, Nil: List[A])((b, a) => Cons(a, b))
+
+  def append[A](l: List[A], r: List[A]): List[A] =
+    foldRight(l, r)(Cons(_, _))
+
+  def foldRight2[A, B](l: List[A], zero: B)(f: (A, B) => B): B =
+    foldLeft(reverse(l), zero)((b: B, a: A) => f(a, b))
+
+  def append2[A](l: List[A], r: List[A]): List[A] =
+    foldRight2(l, r)(Cons(_, _))
+
+  // Not stack-safe.
+  def foldRight3[A, B](l: List[A], zero: B)(f: (A, B) => B): B =
+    foldLeft(l, (b: B) => b)((fn, a) => b => fn(f(a, b)))(zero)
+
+  def concat[A](ls: List[List[A]]): List[A] =
+    foldLeft(ls, Nil: List[A])((ls, l) => append2(ls, l))
+
+  def map[A, B](l: List[A])(f: (A) => B): List[B] =
+    foldRight2(l, Nil: List[B])((a, b) => Cons(f(a), b))
+
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] =
+    foldRight2(l, Nil: List[B])((a, b) => append(f(a), b))
+
+  def zipWith[A, B, C](a: List[A], b: List[B])(f: (A, B) => C): List[C] =
+    (a, b) match {
+      case (_, Nil) => Nil
+      case (Nil, _) => Nil
+      case (Cons(ha, ta), Cons(hb, tb)) => Cons(f(ha, hb), zipWith(ta, tb)(f))
+    }
+
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sup match {
+    case Nil => sub == Nil
+    case Cons(hsup, tsup) => sub match {
+      case Nil => true
+      case Cons(hsub, Nil) => hsup == hsub || hasSubsequence(tsup, sub)
+      case Cons(hsub, tsub) =>
+        if (hsup == hsub) hasSubsequence(tsup, tsub)
+        else hasSubsequence(tsup, sub)
+    }
+  }
+}
---
+++ fp/src/test/scala/functionalDataStructures/list.scala
@@ -0,0 +1,92 @@
+package fp.functionalDataStructures
+
+import org.scalatest._
+
+class FdsSpec extends FlatSpec with Matchers {
+  "tail" should "return the tail of a list" in {
+    val list = List.apply[Int](1, 2, 3)
+    list.tail should equal(List(2, 3))
+    list.tail.tail should equal(List(3))
+    list.tail.tail.tail should be(Nil)
+  }
+
+  "setHead" should "change the head of a list" in {
+    val list = List.apply[Int](1, 2, 3)
+    List.setHead(list, 5) should equal(List(5, 2, 3))
+  }
+
+  "drop" should "remove a number of elements from a list" in {
+    val list = List(1, 2, 3)
+    list.drop(2) should equal(List(3))
+    list.drop(3) should equal(Nil)
+  }
+
+  "dropWhile" should "remove elements from a list while a predicate is true" in {
+    val list = List(1, 2, 3)
+    List.dropWhile(list)(_ < 3) should equal(List(3))
+    List.dropWhile(list)((x) => true) should equal(Nil)
+  }
+
+  "init" should "remove the last element from a list" in {
+    List.init(List(1, 2, 3)) should equal(List(1, 2))
+  }
+
+  "foldRight" should "recreate a list by using Cons" in {
+    List.foldRight(List(1, 2, 3), Nil: List[Int])(Cons(_, _)) should equal(List(1, 2, 3))
+  }
+
+  "length" should "compute the length of a list" in {
+    List.length(List(1, 2, 3)) should equal(3)
+    List.length(Nil) should equal(0)
+  }
+
+  "foldLeft" should "have the same effect as foldRight when f is commutative" in {
+    val list = List(1, 2, 3, 4, 5)
+    List.foldLeft(list, 1)(_ * _) should equal(List.foldRight(list, 1)(_ * _))
+  }
+
+  "foldLeft" should "be used to compute sum, product and length" in {
+    val list = List(1, 2, 3, 4, 5)
+    List.foldLeft(list, 0)(_ + _) should equal(15)
+    List.foldLeft(list, 1)(_ * _) should equal(120)
+    List.foldLeft(list, 0)((b, a) => b + 1) should equal(5)
+  }
+
+  "reverse" should "reverse a list" in {
+    List.reverse(List(1, 2, 3)) should equal(List(3, 2, 1))
+  }
+
+  "foldRigt2" should "work identically to foldRight" in {
+    List.foldRight2(List(1, 2, 3), Nil: List[Int])(Cons(_, _)) should equal(List.foldRight(List(1, 2, 3), Nil: List[Int])(Cons(_, _)))
+  }
+
+  "concat" should "concatenate a list of lists" in {
+    val l1 = List(1, 2, 3)
+    val l2 = List(4, 5, 6)
+    val l3 = List(7, 8, 9)
+
+    List.concat(List(l1, l2, l3)) should equal(List(1, 2, 3, 4, 5, 6, 7, 8, 9))
+  }
+
+  "map" should "apply a mapping fn to all elements of a list" in {
+    List.map(List(1, 2, 3))(_ + 1) should equal(List(2, 3, 4))
+  }
+
+  "flatMap" should "apply a mapping fn to all elements of a list and flattening the result" in {
+    List.flatMap(List(1, 2, 3))((a) => List(a + 1, a + 2)) should equal(List(2, 3, 3, 4, 4, 5))
+  }
+
+  "zipWith" should "be able to add elements of two lists together" in {
+    List.zipWith(List(1, 2, 3), List(4, 5, 6))(_ + _) should equal(List(5, 7, 9))
+  }
+
+  "hasSubsequence" should "find a subsequence within a list" in {
+    List.hasSubsequence(List(1, 2, 3, 4, 5), List(2, 3)) should be(true)
+    List.hasSubsequence(List(1, 2, 3, 4, 5), List(1, 2)) should be(true)
+    List.hasSubsequence(List(1, 2, 3, 4, 5), List(4, 5)) should be(true)
+    List.hasSubsequence(List(1, 2, 3, 4, 5), List(5)) should be(true)
+    List.hasSubsequence(List(1, 2, 3, 4, 5), List(1)) should be(true)
+    List.hasSubsequence(List(1, 2, 3, 4, 5), List(1, 2, 3, 4, 5)) should be(true)
+  }
+}
+
