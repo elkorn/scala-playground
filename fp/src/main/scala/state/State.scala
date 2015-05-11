f3f27f51fd6a674663e0841fe88d632ce5ea1434
Korneliusz Caputa <helluinster@gmail.com>
2015-05-11 20:58:39 +0200

    Add pure state exercises.

--- fp/src/main/scala/state/State.scala
+++ fp/src/main/scala/state/State.scala
@@ -7,12 +7,44 @@ case class State[S, +A](run: S => (A, S)) {
       (f(a), s2)
     }
   )
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    State(s1 => {
+      val (a, s2) = run(s1)
+      val (b, s3) = sb.run(s2)
+
+      (f(a, b), s3)
+    })
+
+  def map2_2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap((a) => sb.map((b) => f(a, b)))
+
+  // Using a for comprehension allows getting rid of explicit state passing completely.
+  def map2_3[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = for {
+    a <- this
+    b <- sb
+  } yield (f(a, b))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    State(s1 => {
+      val (a, s2) = run(s1)
+      f(a).run(s2)
+    })
 }
 
 object State {
   def unit[S, A](a: A): State[S, A] =
     State(s => (a, s))
-}
 
-// // Just making the concept explicit.
-// type State[State, +Result] = State => (Result, State)
+  def map2[S, A, B, C](sa: State[S, A], sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    State(s1 => {
+      println(s"s1: $s1")
+      val (a, s2) = sa.run(s1)
+      println(s"a: $a, s2: $s2")
+      val (b, s3) = sb.run(s2)
+      println(s"b: $b, s3: $s3")
+      val result = (f(a, b), s3)
+      println("map2 is done.")
+      result
+    })
+}
--- fp/src/test/scala/state/StateSpec.scala
+++ fp/src/test/scala/state/StateSpec.scala
@@ -10,6 +10,127 @@ class StateSpec extends FlatSpec with Matchers {
 
   "map" should "apply a mapping fn to a state" in {
     State((s: Int) => (s + 3, s)).map(_ * 2).run(10) should equal((26, 10))
+  }
 
+  "flatMap" should "apply a mapping fn and flatten the result" in {
+    // The first result (of type A) is being discarded, only the state transition persists through all the actions.
+    State((s: Int) => (s + 3, s * 2)).flatMap((a) => State((s: Int) => (s / 3, s * 8))).run(10) should equal((6, 160))
+  }
+
+  def op1(n: Int): Int = {
+    println(s"op1: $n + 3")
+    n + 3
+  }
+  def op2(n: Int): Int = {
+    println(s"op2: $n * 3")
+    n * 3
+  }
+  def op3(n: Int): Int = {
+    println(s"op3: $n + 1")
+    n + 1
+  }
+  def op4(n: Int): Int = {
+    println(s"op4: $n + 8")
+    n + 8
+  }
+
+  def mapOp(a: Int, b: Int) = {
+    println(s"mapOp: $a + $b")
+    a + b
+  }
+
+  def stateOp(n: Int) = {
+    println(s"stateOp: $n % 5")
+    n % 5
+
+  }
+
+  def stateOp1(n: Int) = {
+    println(s"stateOp1: op2($n % 5)")
+    op2(n % 5)
+  }
+
+  def stateOp2(n: Int) = {
+    println(s"stateOp2: op4($n % 5)")
+    op4(n % 5)
+  }
+
+  val expectedResultPipelineA = op1 _
+  val expectedResultPipelineB = stateOp1 _ andThen op3
+  val expectedStatePipeline = stateOp1 _ andThen stateOp2 _
+
+  val state1 = State((s: Int) => (op1(s), stateOp1(s)))
+  val state2 = State((s: Int) => (op3(s), stateOp2(s)))
+  "map2" should "combine two state actions" in {
+    val initialState = 12
+    val expected = (
+      mapOp(
+        expectedResultPipelineA(initialState),
+        expectedResultPipelineB(initialState)
+      ),
+        expectedStatePipeline(initialState)
+    )
+    State.map2(
+      state1,
+      state2
+    )(mapOp).run(initialState) should equal(expected)
+
+    state1.map2(state2)(mapOp).run(initialState) should equal(expected)
+  }
+
+  "map2_2" should "act the same as map2" in {
+    val initialState = 12
+    state1.map2_2(state2)(mapOp).run(initialState) should equal(state1.map2(state2)(mapOp).run(initialState))
+  }
+
+  "map2_3" should "act the same as map2" in {
+    val initialState = 12
+    state1.map2_3(state2)(mapOp).run(initialState) should equal(state1.map2(state2)(mapOp).run(initialState))
+  }
+
+  "coffee example" should "be more explanatory" in {
+    val blackPrice = 4
+    val lattePrice = 7
+    trait Price {
+      val price: Int
+    }
+
+    abstract class Order(val number: Int) extends Price
+
+    case class BlackCoffeeOrder(override val number: Int) extends Order(number) {
+      val price = blackPrice
+    }
+
+    case class LatteOrder(override val number: Int) extends Order(number) {
+      val price = lattePrice
+    }
+
+    sealed trait Coffee
+    case object BlackCoffee extends Coffee
+    case object Latte extends Coffee
+    case class CreditCard(balance: Int) {
+      def charge(amount: Int) = CreditCard(balance - amount)
+    }
+
+    type Coffees = List[Coffee]
+
+    def makeCoffees(order: Order): List[Coffee] = order match {
+      case BlackCoffeeOrder(n) => List.fill(n)(BlackCoffee)
+      case LatteOrder(n) => List.fill(n)(Latte)
+    }
+
+    def fulfillOrder(order: Order): State[CreditCard, Coffees] =
+      State(creditCard => {
+        println(order)
+        println(creditCard)
+        println(order.number)
+        (makeCoffees(order), creditCard.charge(order.number * order.price))
+      })
+
+    fulfillOrder(LatteOrder(2)).run(CreditCard(20)) should equal((List(Latte, Latte), CreditCard(6)))
+    val lattes: State[CreditCard, Coffees] = fulfillOrder(LatteOrder(2))
+    val blackCoffees: State[CreditCard, Coffees] = fulfillOrder(BlackCoffeeOrder(1))
+    val s = State.map2(lattes, blackCoffees)(_ ::: _)
+    s.run(CreditCard(20)) should equal((List(Latte, Latte, BlackCoffee), CreditCard(2)))
   }
 }
