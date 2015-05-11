d98a7d4b8dd9426ed7e39c5e05bcdded4fdb90b2
Korneliusz Caputa <helluinster@gmail.com>
2015-05-11 22:03:03 +0200

    Add more State exercises.

--- fp/src/main/scala/state/State.scala
+++ fp/src/main/scala/state/State.scala
@@ -47,4 +47,38 @@ object State {
       println("map2 is done.")
       result
     })
+
+  // Walks the list once, not tail-recursive
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] =
+    sas.foldRight(unit[S, List[A]](Nil: List[A]))((sa, acc) => sa.map2(acc)(_ :: _))
+
+  // Walks the list twice, not tail-recursive
+  def sequence2[S, A](sas: List[State[S, A]]): State[S, List[A]] = {
+    def go(s0: S, todo: List[State[S, A]], result: List[A]): (List[A], S) = todo match {
+      case h :: t => {
+        val (a, s1) = h.run(s0)
+        go(s1, t, a :: result)
+
+      }
+      case Nil => (result.reverse, s0)
+    }
+
+    State(s => go(s, sas, Nil: List[A]))
+  }
+
+  // Walks the list twice, tail-recursive
+  def sequence3[S, A](sas: List[State[S, A]]): State[S, List[A]] =
+    sas.reverse.foldLeft(unit[S, List[A]](Nil))((acc, sa) => sa.map2(acc)(_ :: _))
+
+  // Promote the state to a result.
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  // Override the state for any given transition.
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  // Use a function to change the state 'in-place', sort of.
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
 }
--- fp/src/test/scala/state/StateSpec.scala
+++ fp/src/test/scala/state/StateSpec.scala
@@ -2,10 +2,11 @@ package fp.state
 
 import org.scalatest._
 import org.scalatest.Matchers._
+import State._
 
 class StateSpec extends FlatSpec with Matchers {
   "unit" should "return a value regardless of state" in {
-    State.unit(2).run(3) should equal((2, 3))
+    unit(2).run(3) should equal((2, 3))
   }
 
   "map" should "apply a mapping fn to a state" in {
@@ -41,28 +42,30 @@ class StateSpec extends FlatSpec with Matchers {
 
   def stateOp(n: Int) = {
     println(s"stateOp: $n % 5")
-    n % 5
+    n + 31
 
   }
 
   def stateOp1(n: Int) = {
     println(s"stateOp1: op2($n % 5)")
-    op2(n % 5)
+    op2(n - 29)
   }
 
   def stateOp2(n: Int) = {
     println(s"stateOp2: op4($n % 5)")
-    op4(n % 5)
+    op4(n + 17)
   }
 
   val expectedResultPipelineA = op1 _
   val expectedResultPipelineB = stateOp1 _ andThen op3
   val expectedStatePipeline = stateOp1 _ andThen stateOp2 _
 
+  val initialState = 12
+
   val state1 = State((s: Int) => (op1(s), stateOp1(s)))
   val state2 = State((s: Int) => (op3(s), stateOp2(s)))
+
   "map2" should "combine two state actions" in {
-    val initialState = 12
     val expected = (
       mapOp(
         expectedResultPipelineA(initialState),
@@ -70,7 +73,7 @@ class StateSpec extends FlatSpec with Matchers {
       ),
         expectedStatePipeline(initialState)
     )
-    State.map2(
+    map2(
       state1,
       state2
     )(mapOp).run(initialState) should equal(expected)
@@ -79,14 +82,56 @@ class StateSpec extends FlatSpec with Matchers {
   }
 
   "map2_2" should "act the same as map2" in {
-    val initialState = 12
     state1.map2_2(state2)(mapOp).run(initialState) should equal(state1.map2(state2)(mapOp).run(initialState))
   }
 
   "map2_3" should "act the same as map2" in {
-    val initialState = 12
     state1.map2_3(state2)(mapOp).run(initialState) should equal(state1.map2(state2)(mapOp).run(initialState))
   }
+
+  "sequence" should "perform state transitions while keeping the intermediate results" in {
+    val expectedResult1 = state1.run(initialState)
+    val expectedResult2 = state2.run(expectedResult1._2)
+    val expectedState = state1.map2(state2)((_, _) => 42).run(initialState)._2 // the result doesn't really matter here.
+    sequence(List(state1, state2)).run(initialState) should equal(List(expectedResult1._1, expectedResult2._1), expectedState)
+  }
+
+  "sequence2" should "act the same as sequence" in {
+    sequence2(List(state1, state2)).run(initialState) should equal(sequence(List(state1, state2)).run(initialState))
+  }
+
+  "sequence3" should "act the same as sequence" in {
+    sequence3(List(state1, state2)).run(initialState) should equal(sequence(List(state1, state2)).run(initialState))
+  }
+
+  "get" should "promote the state value to a result" in {
+    val expected = state2.run(initialState)
+    get.run(state2.run(initialState)) should equal((expected, expected))
+    get.run(initialState) should equal((initialState, initialState))
+    (for {
+      _ <- modify[Int](_ * 2)
+      z <- get[Int]
+    } yield z).run(initialState) should equal((initialState * 2, initialState * 2))
+  }
+
+  "set" should "lift any value to state value" in {
+    set(123).run(initialState) should equal(((), 123))
+    // This gets the modified state and then sets it to 13.
+    (for {
+      _ <- modify[Int](_ * 2)
+      z <- get[Int]
+      _ <- set(13)
+    } yield z).run(initialState) should equal((initialState * 2, 13))
+  }
+
+  "modify" should "change the state value of a transition" in {
+    (for {
+      x <- state2
+      y <- modify[Int](_ + 18)
+    } yield y).run(initialState) should equal(((), state2.run(initialState)._2 + 18))
+  }
+
+  // EXAMPLES
 
   "coffee example" should "be more explanatory" in {
     val blackPrice = 4
@@ -130,7 +175,7 @@ class StateSpec extends FlatSpec with Matchers {
     fulfillOrder(LatteOrder(2)).run(CreditCard(20)) should equal((List(Latte, Latte), CreditCard(6)))
     val lattes: State[CreditCard, Coffees] = fulfillOrder(LatteOrder(2))
     val blackCoffees: State[CreditCard, Coffees] = fulfillOrder(BlackCoffeeOrder(1))
-    val s = State.map2(lattes, blackCoffees)(_ ::: _)
+    val s = map2(lattes, blackCoffees)(_ ::: _)
     s.run(CreditCard(20)) should equal((List(Latte, Latte, BlackCoffee), CreditCard(2)))
   }
 }
