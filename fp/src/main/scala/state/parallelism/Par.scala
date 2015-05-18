e10b0431c888b2b77564f403f6cc150cbe1860ac
Korneliusz Caputa <helluinster@gmail.com>
2015-05-18 22:13:51 +0200

    Use Java Futures as the backing mechanism.

--- fp/src/main/scala/state/parallelism/Par.scala
+++ fp/src/main/scala/state/parallelism/Par.scala
@@ -1,19 +1,88 @@
 package fp.state.parallelism
 
-case class Par[A](value: A)
+import java.util.concurrent.{ Future, ExecutorService, Callable, TimeUnit }
+import scala.concurrent.duration._
+
+package object parallelism {
+  type Par[A] = ExecutorService => Future[A]
+}
 
 object Par {
-  def unit[A](a: => A): Par[A] = Par(a)
-  def get[A](par: Par[A]): A = par.value
+  import parallelism._
 
-  def sum(ints: IndexedSeq[Int]): Par[Int] = {
-    if (ints.length <= 1) unit(ints.headOption.getOrElse(0))
-    else {
-      val (l, r) = ints.splitAt(ints.length / 2)
-      map2(sum(l), sum(r))(_ + _)
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  private case class Map2Future[A, B, C](fa: Future[A], fb: Future[B], f: (A, B) => C) extends Future[C] {
+    @volatile var cache: Option[C] = None
+
+    def isDone = cache.isDefined
+    def isCancelled = fa.isCancelled || fb.isCancelled
+    def cancel(evenIfRunning: Boolean) =
+      fa.cancel(evenIfRunning) || fa.cancel(evenIfRunning)
+    def get = compute(Long.MaxValue)
+    def get(timeout: Long, unit: TimeUnit): C =
+      compute(TimeUnit.MILLISECONDS.convert(timeout, unit))
+
+    private def compute(timeoutMs: Long): C = cache match {
+      case Some(c) => c
+      case None =>
+        val start = System.currentTimeMillis
+        val ar = fa.get(timeoutMs, TimeUnit.MILLISECONDS)
+        val stop = System.currentTimeMillis
+        val at = stop - start
+        val br = fb.get(timeoutMs - at, TimeUnit.MILLISECONDS)
+        val ret = (f(ar, br))
+        cache = Some(ret)
+        ret
     }
   }
 
-  def map2[A, B, C](pa: => Par[A], pb: => Par[B])(f: (A, B) => C): Par[C] =
-    unit(f(get(pa), get(pb)))
+  def unit[A](a: A): Par[A] = es => UnitFuture(a)
+  // A derived combinator - one that is expressed wholly in terms of other operations.
+  // It does not need to know anything about the representation of Par.
+  def lazyUnit[A](a: => A) = ??? // fork(unit(a))
+
+  def asyncF[A, B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
+
+  /*
+   Allows the user of the library to specify explicitly that a
+   computation should be run on a separate logical thread.
+   map2 can be made strict, since the decision of forking is left to the user.
+   */
+  // def fork[A](a: => Par[A]): Par[A] =
+  //   es => es.submit(new Callable[A] {
+  //     def call = a(es).get
+  //   })
+
+  /*
+   Extracts the value from a computation by performing it.
+   Returning a Future gives more control to the user by leaving timeouts, cancellation etc. up to her.
+   */
+  def run[A](es: ExecutorService)(a: Par[A]): Future[A] = a(es)
+
+  // def sum(ints: IndexedSeq[Int]): Par[Int] = {
+  //   if (ints.length <= 1) unit(ints.headOption.getOrElse(0))
+  //   else {
+  //     val (l, r) = ints.splitAt(ints.length / 2)
+  //     map2(Par.fork(sum(l)), Par.fork(sum(r)))(_ + _)
+  //   }
+  // }
+
+  /*
+   The function f does is not evaluated on a separate logical thread.
+   To achieve that, one might use fork(map2(a,b)(f)).
+   */
+  def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] =
+    es => {
+      val af = pa(es)
+      val bf = pb(es)
+      Map2Future(af, bf, f)
+    }
+
 }
--- fp/src/test/scala/state/parallelism/ParSpec.scala
+++ fp/src/test/scala/state/parallelism/ParSpec.scala
@@ -5,13 +5,13 @@ import org.scalatest._
 import Par._
 
 class ParSpec extends FlatSpec with Matchers {
-  "sum" should "sum a sequence of integers" in {
-    sum(IndexedSeq(1, 2, 3, 4, 5)) should equal(Par(1 + 2 + 3 + 4 + 5))
+  // "sum" should "sum a sequence of integers" in {
+  //   sum(IndexedSeq(1, 2, 3, 4, 5)) should equal(Par.unit(1 + 2 + 3 + 4 + 5))
 
-  }
+  // }
 
-  "map2" should "map two parallel computations together" in {
-    map2(unit(123), unit(44))(_ + _) should equal(unit(123 + 44))
-  }
+  // "map2" should "map two parallel computations together" in {
+  //   map2(unit(123), unit(44))(_ + _) should equal(unit(123 + 44))
+  // }
 
 }
