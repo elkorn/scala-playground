b2d98c932ea8443f582f502ab23b43d9a7a7750b
Korneliusz Caputa <helluinster@gmail.com>
2015-05-18 21:14:52 +0200

    Use map2 for combining parallel computations.
    This allows to push using `get` back to the furthest possible moment.

--- fp/src/main/scala/state/parallelism/Par.scala
+++ fp/src/main/scala/state/parallelism/Par.scala
@@ -6,13 +6,14 @@ object Par {
   def unit[A](a: => A): Par[A] = Par(a)
   def get[A](par: Par[A]): A = par.value
 
-  def sum(ints: IndexedSeq[Int]): Int = {
-    if (ints.length <= 1) ints.headOption.getOrElse(0)
+  def sum(ints: IndexedSeq[Int]): Par[Int] = {
+    if (ints.length <= 1) unit(ints.headOption.getOrElse(0))
     else {
       val (l, r) = ints.splitAt(ints.length / 2)
-      val sumL = Par.unit(sum(l))
-      val sumR = Par.unit(sum(r))
-      Par.get(sumL) + Par.get(sumR)
+      map2(sum(l), sum(r))(_ + _)
     }
   }
+
+  def map2[A, B, C](pa: => Par[A], pb: => Par[B])(f: (A, B) => C): Par[C] =
+    unit(f(get(pa), get(pb)))
 }
--- fp/src/test/scala/state/parallelism/ParSpec.scala
+++ fp/src/test/scala/state/parallelism/ParSpec.scala
@@ -6,8 +6,12 @@ import Par._
 
 class ParSpec extends FlatSpec with Matchers {
   "sum" should "sum a sequence of integers" in {
-    sum(IndexedSeq(1, 2, 3, 4, 5)) should equal(1 + 2 + 3 + 4 + 5)
+    sum(IndexedSeq(1, 2, 3, 4, 5)) should equal(Par(1 + 2 + 3 + 4 + 5))
 
+  }
+
+  "map2" should "map two parallel computations together" in {
+    map2(unit(123), unit(44))(_ + _) should equal(unit(123 + 44))
   }
 
 }
